8499b2a51e618bba5735a8861f330131e2f2f3fc
Tri <tductri3@gmail.com>
2023-12-17 18:37:02 +0700

    push code

---
+++ src/global/styles.js
@@ -0,0 +1,27 @@
+export const colors = {
+    buttons: "#ff8c52",
+    grey1: '#43484d',
+    grey2: '#5e6977',
+    grey3: '#86939e',
+    grey4: '#bdc6cf',
+    grey5: '#1e8ee',
+    CardComment: '#86939e',
+    cardbackground: "white",
+    statusbar: "#ff8c52",
+    headerText: "white",
+}
+
+export const parameters = {
+    headerHeight: 40,
+}
+
+// export const title = {
+//     container: {
+//         flex:1
+//     },
+
+//     text1: {
+//         color: colors.grey3,
+//         fontSize: 16
+//     },
+// })
---
+++ src/screens/authScreens/SignInScreen.js
@@ -0,0 +1,103 @@
+import React, {useState, useRef} from 'react';
+import {View, Text, StyleSheet, Dimensions, TextInput} from 'react-native';
+import {colors, parameters, title} from "../../global/styles";
+import { Icon } from 'react-native-elements';
+import Header from '../../components/Header';
+import * as Animatable from 'react-native-animatable';
+ export default function SignInScreen() {
+
+    const[textInput2Foccued, setTextInput2Foccued] = useState(false)
+
+    const textInput1 = useRef(1)
+    const textInput2 = useRef(2)
+
+    return (
+        <View style = {styles.container}>
+            <Header title="MY ACCOUNT" type = "arrow-left"/>
+
+            <View style = {{marginLeft: 20, marginTop: 10}}>
+                <Text style = {title}>Sign-In</Text>
+            </View>
+
+            <View style = {{alignItems: "center",marginTop: 10}}>
+                <Text style = {styles.text1}>Hãy nhập email và password</Text>
+                <Text style = {styles.text1}>đã đăng ký với tài khoản của bạn</Text>
+            </View>
+
+            <View style = {{marginTop: 20}}>
+                <View>
+                    <TextInput 
+                        style = {styles.TextInput1}
+                        placeholder = "Email" 
+                        ref = {textInput1}              
+                    />
+                </View>
+
+                <View style = {styles.TextInput2}>
+                    <Animatable.View  animation = {textInput2Foccued ? "" : "fadeInLeft"} duration = {400}>
+                        <Icon 
+                            name = "lock"
+                            iconStyle = {{color: colors.grey3}}
+                            type = "material"
+                            style = {{}}
+                        />
+                    </Animatable.View>
+
+                    <TextInput 
+                        style = {{width: "80%"}}
+                        placeholder = "Password"       
+                        ref = {textInput2}      
+                        onFocus= {()=>{
+                            setTextInput2Foccued(false)
+                        }}
+
+                        onBlur = {()=>{
+                            setTextInput2Foccued(true)
+                        }}
+                    />
+
+                    <Animatable.View animation = {textInput2Foccued ? "" : "fadeInLeft"} duration = {400}>
+                        <Icon 
+                            name = "visibility-off"
+                            iconStyle = {{color: colors.grey3}}
+                            type = "material"
+                            style = {{marginRight: 10}}
+                        />
+                    </Animatable.View>
+                </View>
+            </View>
+        </View>
+    )
+ }
+
+ const styles = StyleSheet.create({
+    container: {
+        flex:1
+    },
+
+    text1: {
+        color: colors.grey3,
+        fontSize: 16
+    },
+
+    TextInput1: {
+        borderWidth: 1,
+        borderColor: "#86939e",
+        marginHorizontal: 20,
+        borderRadius: 12,
+        marginBottom: 20,
+        paddingLeft: 15
+    },
+
+    TextInput2: {
+        borderWidth: 1,
+        borderRadius: 12,
+        marginHorizontal: 20,
+        borderColor: "#86939e",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        alignItems: "center",
+        paddingLeft: 15
+    }
+ })
