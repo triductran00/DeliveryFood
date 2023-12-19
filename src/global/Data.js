912fe3fa60435f6206ed88a38a55cbcefe281f2d
Tri <tductri3@gmail.com>
2023-12-19 23:41:30 +0700

    Update Categories Card

---
+++ src/global/Data.js
@@ -0,0 +1,8 @@
+export const filterData = [
+    {name: "Đồ ăn nhanh", image: require('../assets/fastfood.png'), id: "0"},
+    {name: "Ăn vặt", image: require('../assets/snack.png'), id: "1"},
+    {name: "Đồ uống", image: require('../assets/drink.png'), id: "2"},
+    {name: "Cơm", image: require('../assets/rice.png'), id: "3"},
+    {name: "Ăn chay", image: require('../assets/vegetarianfood.png'), id: "4"},
+    {name: "Hải sản", image: require('../assets/seafood.png'), id: "5"},
+];
--- src/screens/authScreens/HomeScreen.js
+++ src/screens/authScreens/HomeScreen.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList, Pressable, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import HomeHeader from '../../components/HomeHeader';
 import { colors, parameters } from '../../global/styles';
+import { filterData } from '../../global/Data';
 
 export default function HomeScreen() {
 
     const [delivery, setDelivery] = useState(true);
-
+    const [indexCheck, setIndexCheck] = useState("0")
     return ( 
         <View style = {styles.container}>
             <HomeHeader />
@@ -78,6 +79,38 @@ export default function HomeScreen() {
                 <View style = {styles.headerTextView}>
                     <Text style = {styles.headerText}>Phân loại</Text>
                 </View>
+
+                <View>
+                    <FlatList 
+                        horizontal = {true}
+                        showsHorizontalScrollIndicator = {false}
+                        data= {filterData}
+                        keyExtractor = {(item)=>item.id}
+                        extraData = {indexCheck}
+                        renderItem = {({item, index})=>(
+                            <Pressable
+                                onPress = {()=>{setIndexCheck(item.id)}}
+                            >
+                                <View style = {indexCheck === item.id ? {...styles.smallCardSelected} : {...styles.smallCard}}>
+                                    <Image 
+                                        style = {{height: 60, width: 60, borderRadius: 30}}
+                                        source = {item.image}
+                                    />
+
+                                    <View>
+                                        <Text style = {indexCheck === item.id ? {...styles.smallCardTextSelected} : {...styles.smallCardText}}>{item.name}</Text>
+                                    </View>
+                                </View>
+
+                               
+                            </Pressable>
+                        )}
+                    />
+                </View>
+
+                <View style = {styles.headerTextView}>
+                    <Text style = {styles.headerText}>Freeship</Text>
+                </View>
             </ScrollView>
         </View>
     )
@@ -138,5 +171,38 @@ const styles = StyleSheet.create({
     headerTextView: {
         backgroundColor: colors.grey5,
         paddingVertical: 2,
+    },
+
+    smallCard: {
+        borderRadius: 30,
+        backgroundColor: colors.grey5,
+        justifyContent: "center",
+        alignItems: 'center',
+        padding: 5,
+        widh: 80,
+        margin: 10,
+        height: 100
+    },
+
+    smallCardSelected: {
+        borderRadius: 30,
+        backgroundColor: colors.buttons,
+        justifyContent: "center",
+        alignItems: 'center',
+        padding: 5,
+        widh: 80,
+        margin: 10,
+        height: 100
+    },
+
+    smallCardTextSelected: {
+        fontWeight: "bold",
+        color: colors.cardbackground
+    },
+
+    smallCardText: {
+        fontWeight: "bold",
+        color: colors.grey2
+
     }
 }) 
