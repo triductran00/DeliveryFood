e12726e868bc546d4425e0cad273dcf3e9d4da95
Tri <tductri3@gmail.com>
2023-12-23 16:00:04 +0700

    Update SignInScreen

--- src/screens/authScreens/SignInScreen.js
+++ src/screens/authScreens/SignInScreen.js
@@ -5,13 +5,11 @@ import * as Animatable from 'react-native-animatable';
 import { Icon, Button, SocialIcon} from 'react-native-elements';
 import { Formik } from 'formik';
 import Header from '../../components/Header';
-<<<<<<< HEAD
 import DrawerNavigator from '../../navigation/DrawerNavigator';
 import HomeScreen from './HomeScreen';
-=======
 import auth from '@react-native-firebase/auth';
 import { SignInContext } from '../../contexts/authContext';;
->>>>>>> 729380cedb7041e75ea51104e529e56167551437
+
 
 export default function SignInScreen({navigation}){
 
